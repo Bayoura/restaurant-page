f8aec01e2be3e9304d8265ff833d5927836e6bad
Bayoura <Kristin.Seidelmann@gmx.de>
2022-02-21 14:25:17 +0100

    add main element

--- src/load-home.js
+++ src/load-home.js
@@ -6,6 +6,7 @@ function loadHomepage() {
 }
 
 function createHomepage() {
+    const main = document.createElement('main');
     const welcomeBox_parent = document.createElement('div');
     welcomeBox_parent.classList.add('welcome-box');
     const heading2 = document.createElement('h2');
@@ -15,7 +16,8 @@ function createHomepage() {
     button.type = 'button';
     
     welcomeBox_parent.append(heading2, createParagraph('Whatever ailment plagues you, be assured, we got a cure. Our treatments range from medicinal teas over healing herbs and soothing ointments to magic potions.'), createParagraph('Whether you suffer from a headache, a flesh wound or food poisoning — we have the remedy to provide you with sweet relief.'), button);
-    return welcomeBox_parent;
+    main.append(welcomeBox_parent);
+    return main;
 }
 
 function createParagraph(text) {
