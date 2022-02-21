421ac1026b695145b47a49c7cad6451ed2bdef2f
Bayoura <Kristin.Seidelmann@gmx.de>
2022-02-21 14:06:38 +0100

    add initial page load and menu event listeners

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import './styles.css';
-import createPage from './page-load.js';
+import loadPage from './load-page.js';
+import loadHomepage from './load-home.js';
+import loadRemedies from './load-remedies.js';
 
-createPage();
+loadInitialPage();
+
+function loadInitialPage() {
+    loadPage();
+    loadHomepage();
+    addMenuEvents();
+}
+
+function addMenuEvents() {
+    const remedies = document.getElementById('remedies');
+    const about = document.getElementById('about');
+    const contact = document.getElementById('contact');
+    remedies.addEventListener('click', loadRemedies);
+    about.addEventListener('click', loadAbout);
+    contact.addEventListener('click', loadContact);
+}
