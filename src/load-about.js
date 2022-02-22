064cd46d6280bd62639f9a2c1f4be7de0f655fbf
Bayoura <Kristin.Seidelmann@gmx.de>
2022-02-22 12:10:09 +0100

    add function for testing

--- src/load-about.js
+++ src/load-about.js
@@ -1,1 +1,13 @@
 export default loadAbout;
+
+function loadAbout() {
+    const main = document.querySelector('main');
+    main.textContent = '';
+    return main.append(createRemedyOptions());
+}
+
+function createRemedyOptions() {
+    const heading = document.createElement('h1');
+    heading.innerText = 'About';
+    return heading;
+}
--- src/load-contact.js
+++ src/load-contact.js
@@ -1,1 +1,13 @@
 export default loadContact;
+
+function loadContact() {
+    const main = document.querySelector('main');
+    main.textContent = '';
+    return main.append(createRemedyOptions());
+}
+
+function createRemedyOptions() {
+    const heading = document.createElement('h1');
+    heading.innerText = 'Contact';
+    return heading;
+}
