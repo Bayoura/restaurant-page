eea5bbd85ed4120a62fbfad6fa2ea2665db2f463
Bayoura <Kristin.Seidelmann@gmx.de>
2022-02-22 12:11:35 +0100

    add event listeners for nav buttons

--- src/index.js
+++ src/index.js
@@ -2,20 +2,55 @@ import './styles.css';
 import loadPage from './load-page.js';
 import loadHomepage from './load-home.js';
 import loadRemedies from './load-remedies.js';
+import loadAbout from './load-about.js';
+import loadContact from './load-contact';
 
 loadInitialPage();
 
 function loadInitialPage() {
     loadPage();
     loadHomepage();
+    const logo = document.querySelector('.logo-container');
+    logo.classList.add('current');
     addMenuEvents();
 }
 
 function addMenuEvents() {
+    const logo = document.querySelector('.logo-container');
     const remedies = document.getElementById('remedies');
     const about = document.getElementById('about');
     const contact = document.getElementById('contact');
-    remedies.addEventListener('click', loadRemedies);
-    about.addEventListener('click', loadAbout);
-    contact.addEventListener('click', loadContact);
+
+    logo.addEventListener('click', button => {
+        setCurrentClass(button.currentTarget);
+        if (button.currentTarget.classList.contains('current')) loadHomepage();
+    })
+
+    remedies.addEventListener('click', button => {
+        setCurrentClass(button.target);
+        if (button.target.classList.contains('current')) loadRemedies();
+    });
+    about.addEventListener('click', button => {
+        setCurrentClass(button.target);
+        if (button.target.classList.contains('current')) loadAbout();
+    });
+    contact.addEventListener('click', button => {
+        setCurrentClass(button.target);
+        if (button.target.classList.contains('current')) loadContact();
+    }); 
+}
+
+function setCurrentClass(clickedButton) {
+    if (clickedButton.classList.contains('current')) return;
+
+    const logo = document.querySelector('.logo-container');
+    if (logo !== clickedButton) {
+        logo.classList.remove('current');
+    }
+
+    const buttons = document.querySelectorAll('.nav-link');
+    buttons.forEach(button => {
+        if (button != clickedButton) button.classList.remove('current');
+    });
+    clickedButton.classList.add('current');
 }
