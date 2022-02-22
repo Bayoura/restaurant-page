610ef87725af709d1b3a2d5548caf7147304084a
Bayoura <Kristin.Seidelmann@gmx.de>
2022-02-22 13:58:25 +0100

    change box name

--- src/load-home.js
+++ src/load-home.js
@@ -8,8 +8,8 @@ function loadHomepage() {
 }
 
 function createHomepage() {
-    const welcomeBox_parent = document.createElement('div');
-    welcomeBox_parent.classList.add('welcome-box');
+    const box_parent = document.createElement('div');
+    box_parent.classList.add('box');
     const heading2 = document.createElement('h2');
     heading2.innerText = 'Welcome, weary traveller!';
     const button = document.createElement('button');
@@ -21,9 +21,9 @@ function createHomepage() {
         loadRemedies();
     });
     
-    welcomeBox_parent.append(heading2, createParagraph('Whatever ailment plagues you, be assured, we got a cure. Our treatments range from medicinal teas over healing herbs and soothing ointments to magic potions.'), createParagraph('Whether you suffer from a headache, a flesh wound or food poisoning — we have the remedy to provide you with sweet relief.'), button);
+    box_parent.append(heading2, createParagraph('Whatever ailment plagues you, be assured, we got a cure. Our treatments range from medicinal teas over healing herbs and soothing ointments to magic potions.'), createParagraph('Whether you suffer from a headache, a flesh wound or food poisoning — we have the remedy to provide you with sweet relief.'), button);
     
-    return welcomeBox_parent;
+    return box_parent;
 }
 
 function createParagraph(text) {
