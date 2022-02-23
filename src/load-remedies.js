c1a794886d8281c14e87c318b3e77909c5530e4f
Bayoura <Kristin.Seidelmann@gmx.de>
2022-02-23 12:34:23 +0100

    sort items, add two-span class

--- src/load-remedies.js
+++ src/load-remedies.js
@@ -14,17 +14,17 @@ function createRemedyPage() {
     const itemContainer = document.createElement('div');
     itemContainer.classList.add('item-container');
 
-    const item1 = createRemedyItem('../src/images/assortment.jpg', 'green paste');
-    const item2 = createRemedyItem('../src/images/green-paste.jpg', 'green paste');
-    const item3 = createRemedyItem('../src/images/spoons.jpg', 'green paste');
-    const item4 = createRemedyItem('../src/images/herbal-tea.jpg', 'green paste');
-    const item5 = createRemedyItem('../src/images/flasks.jpg', 'green paste');
+    const item1 = createRemedyItem('../src/images/assortment.jpg', 'green paste', 'span-two');
+    const item2 = createRemedyItem('../src/images/flasks.jpg', 'green paste');
+    const item3 = createRemedyItem('../src/images/mortar-pestle.jpg', 'green paste');
+    const item4 = createRemedyItem('../src/images/black-tea.jpg', 'green paste');
+    const item5 = createRemedyItem('../src/images/herbal-tea.jpg', 'green paste', 'span-two');
     const item6 = createRemedyItem('../src/images/hanging-herbs.jpg', 'green paste');
-    const item7 = createRemedyItem('../src/images/herbs-cup.jpg', 'green paste');
-    const item8 = createRemedyItem('../src/images/mortar-pestle.jpg', 'green paste');
-    const item9 = createRemedyItem('../src/images/red-tea.jpg', 'green paste');
-    const item10 = createRemedyItem('../src/images/tools.jpg', 'green paste');
-    const item11 = createRemedyItem('../src/images/black-tea.jpg', 'green paste');
+    const item7 = createRemedyItem('../src/images/green-paste.jpg', 'green paste', 'span-two');
+    const item8 = createRemedyItem('../src/images/red-tea.jpg', 'green paste');
+    const item9 = createRemedyItem('../src/images/tools.jpg', 'green paste');
+    const item10 = createRemedyItem('../src/images/spoons.jpg', 'green paste', 'span-two');
+    const item11 = createRemedyItem('../src/images/herbs-cup.jpg', 'green paste');
     
     itemContainer.append(item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11);
     box_parent.append(itemContainer);
@@ -32,7 +32,7 @@ function createRemedyPage() {
     return box_parent;
 }
 
-function createRemedyItem(url, caption) {
+function createRemedyItem(url, caption, optionalClass) {
     const figure = document.createElement('figure');
     const image = document.createElement('img');
     image.classList.add('remedy-image')
@@ -42,6 +42,8 @@ function createRemedyItem(url, caption) {
     figureCaption.innerText = caption;
 
     figure.append(image, figureCaption);
+    if (optionalClass) figure.classList.add(optionalClass);
+    
     return figure;
 }
 
